b05728a2154bb81a56e362dfd0f56e4a7bc12c30
DaimZahoor <123406647+devdaim6@users.noreply.github.com>
2024-07-19 15:24:10 +0530

    Contact US Api

---
+++ models/Contact.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const ContactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Contact =
+  mongoose.models.Contact || mongoose.model("Contact", ContactSchema);
+
+export default Contact;

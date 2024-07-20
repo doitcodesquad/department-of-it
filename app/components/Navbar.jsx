c3069ca5fa5c8abf6823bdf67319e8149fdf936a
Naveed-SNR <sheeikhnaveed3660@gmail.com>
2024-07-20 11:38:42 +0530

    Fixed sidebar overflow, used framer motion to animate sidebar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,35 @@
 "use client";
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
+import { motion } from 'framer-motion';
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    if (isOpen) {
+      setIsVisible(true);
+    } else {
+      const timer = setTimeout(() => setIsVisible(false), 300); // match this with your transition duration
+      return () => clearTimeout(timer);
+    }
+  }, [isOpen]);
+
+  const variants = {
+    open: {
+      x: 0,
+      transition: {  
+        damping: 0,
+      },
+    },
+    closed: {
+      x: "100%",
+      transition: {  
+        damping: 0,
+      },
+    },
+  }
 
   return (
     <>
@@ -14,24 +40,24 @@ export default function Navbar() {
           </div>
         </Link>
         <div className="nav-toggle">
-          <button 
+          <button
             className="p-2 text-black focus:outline-none"
             onClick={() => setIsOpen(!isOpen)}
           >
-            <svg 
-              className={`w-6 h-6 ${isOpen ? 'hidden' : 'block'}`} 
-              fill="none" 
-              stroke="currentColor" 
-              viewBox="0 0 24 24" 
+            <svg
+              className={`w-6 h-6 ${isOpen ? 'hidden' : 'block'}`}
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
             </svg>
-            <svg 
-              className={`w-6 h-6 ${isOpen ? 'block' : 'hidden'}`} 
-              fill="none" 
-              stroke="currentColor" 
-              viewBox="0 0 24 24" 
+            <svg
+              className={`w-6 h-6 ${isOpen ? 'block' : 'hidden'}`}
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
@@ -44,24 +70,35 @@ export default function Navbar() {
           <Link className="nav-item" href="/">Clubs</Link>
           <Link className="nav-item" href="/">Contact</Link>
         </div>
-        <div className={`mobile-nav-list ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
-          <div className="flex justify-end p-4">
-            <button 
-              className="text-black focus:outline-none"
-              onClick={() => setIsOpen(false)}
-            >
-              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
-              </svg>
-            </button>
-          </div>
-          <div className="flex flex-col items-center mt-10 space-y-4">
-            <Link className="nav-item-mobile" href="/">Events</Link>
-            <Link className="nav-item-mobile" href="/">Blog</Link>
-            <Link className="nav-item-mobile" href="/">Clubs</Link>
-            <Link className="nav-item-mobile" href="/">Contact</Link>
-          </div>
-        </div>
+        {isVisible && (
+          <motion.div
+            initial="closed"
+            animate={isOpen ? "open" : "closed"}
+            variants={variants}
+            className={`mobile-nav-list ${isOpen ? 'opacity-100' : 'opacity-0'}`}
+            style={{
+              pointerEvents: isOpen ? 'auto' : 'none',
+              transition: 'opacity 0.38s',
+            }}
+          >
+            <div className="flex justify-end p-4">
+              <button
+                className="text-black focus:outline-none"
+                onClick={() => setIsOpen(false)}
+              >
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+                </svg>
+              </button>
+            </div>
+            <div className="flex flex-col items-center mt-10 space-y-4">
+              <Link className="nav-item-mobile" href="/">Events</Link>
+              <Link className="nav-item-mobile" href="/">Blog</Link>
+              <Link className="nav-item-mobile" href="/">Clubs</Link>
+              <Link className="nav-item-mobile" href="/">Contact</Link>
+            </div>
+          </motion.div>
+        )}
       </nav>
     </>
   )

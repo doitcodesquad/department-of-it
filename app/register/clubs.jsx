2558cf0bf6ceb11cb7394bd6e2bbae21210d204c
daimdev6 <daimdev6@gmail.com>
2025-03-16 14:36:18 +0530

    Register Page Update

--- app/register/clubs.jsx
+++ app/register/clubs.jsx
@@ -3,34 +3,24 @@ import React, { useState, useEffect } from 'react';
 import { Icon } from '@iconify/react';
 import { motion } from 'framer-motion';
 
-const Clubs = ({ onNext, onPrevious, data }) => {
+const Clubs = ({ onNext, onPrevious, data, isLastStep }) => {
   const [selectedOptions, setSelectedOptions] = useState(data?.selectedOptions || []);
 
   const options = [
     {
-      name: "Web Development",
-      description: "Create and maintain websites and web applications",
-      icon: "mdi:web"
+      name: "Ignite Startup Club",
+      description: "Develop and grow new business ideas in the tech industry",
+      icon: "mdi:rocket-launch"
     },
     {
-      name: "Research Club",
-      description: "Explore and conduct research in various tech fields. ",
-      icon: "uil:microscope"
-    },
-    {
-      name: "Design",
-      description: "Create visually appealing and functional designs",
-      icon: "mdi:palette"
-    },
-    {
-      name: "Cyber Security",
+      name: "Cybersecurity Club",
       description: "Protect systems, networks, and programs from digital attacks",
       icon: "mdi:shield-lock"
     },
     {
-      name: "Innovative Startups",
-      description: "Develop and grow new business ideas in the tech industry",
-      icon: "mdi:rocket-launch"
+      name: "Research Club",
+      description: "Explore and conduct research in various tech fields",
+      icon: "uil:microscope"
     },
     {
       name: "Gate Club",
@@ -62,45 +52,48 @@ const Clubs = ({ onNext, onPrevious, data }) => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className="min-h-[90vh] bg-emerald-50 p-4 md:p-2">
+      className="min-h-[90vh] bg-emerald-50 p-4 md:p-2"
+    >
       <div className="md:px-8 px-0">
-        <h2 className='text-center my-6'>Select your interests</h2>
-        <div className="max-w-full sm:max-w-3xl mx-auto py-3 sm:py-9 px-2 sm:px-6 bg-orange-50 border-4 border-black relative ">
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 sm:gap-4 pb-4 sm:pb-8 ">
+        <h2 className="text-center my-6">Select your interests</h2>
+        <div className="max-w-full sm:max-w-3xl mx-auto py-3 sm:py-9 px-2 sm:px-6 bg-orange-50 border-4 border-black relative">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 sm:gap-4 pb-4 sm:pb-8">
             {options.map(option => (
-              <div  key={option.name}
-              onClick={() => handleOptionChange(option.name)} className="border-2 border-black ">
-                <div className={`h-24 flex flex-row gap-3 items-center  md:py-1 bg-emerald-50 border transition-all duration-300 cursor-pointer hover:bg-sky-50
-                  ${
-                    selectedOptions.includes(option.name) ? 'text-black border-l-8 border-l-accent' : ''
-                  }`}>
-
-                    <div className={` ms-1 text-3xl md:text-4xl transition-colors duration-300 `}>
-                      <Icon
-                        icon={option.icon}
-                        
-                      />
+              <div
+                key={option.name}
+                onClick={() => handleOptionChange(option.name)}
+                className={`border-2 border-black cursor-pointer transition-all duration-300 ${
+                  selectedOptions.includes(option.name)
+                    ? 'bg-sky-50 border-l-8 border-l-accent'
+                    : 'bg-emerald-50'
+                }`}
+              >
+                <div className="h-24 flex flex-row gap-3 items-center md:py-1">
+                  <div className="ms-1 text-3xl md:text-4xl">
+                    <Icon icon={option.icon} />
+                  </div>
+                  <div className="flex flex-col">
+                    <div className="text-lg md:text-xl">{option.name}</div>
+                    <div>
+                      <p className="text-sm md:text-md opacity-80 line-clamp-3">
+                        {option.description}
+                      </p>
                     </div>
-                    <div className="flex flex-col">
-                      <div className="text-lg md:text-xl">{option.name}</div>
-                        <div className="">
-                          <p className="text-sm md:text-md opacity-80 line-clamp-3">{option.description}</p>
-                        </div>
-                    </div>
-    
-         
+                  </div>
                 </div>
               </div>
             ))}
           </div>
-          <div className=" flex md:flex-nowrap flex-wrap w-full justify-between items-center ">
-            <div className="my-2 me-2 text-[10px] sm:text-sm text-gray-600 order-1 sm:order-none">Choose all that apply</div>
+          <div className="flex md:flex-nowrap flex-wrap w-full justify-between items-center">
+            <div className="my-2 me-2 text-[10px] sm:text-sm text-gray-600 order-1 sm:order-none">
+              Choose all that apply
+            </div>
             <div className="flex justify-end gap-2 items-center w-full md:w-[21.8rem]">
               <button
                 onClick={onPrevious}
                 className="w-full flex items-center justify-center bg-primary cursor-pointer py-2 px-2 sm:px-3 transition-all hover:bg-black hover:text-white duration-300 border-2 border-black group"
               >
-                <Icon 
+                <Icon
                   icon="mdi:arrow-left"
                   className="transition-colors duration-300 text-black group-hover:text-white text-lg"
                   width="14"
@@ -110,10 +103,10 @@ const Clubs = ({ onNext, onPrevious, data }) => {
               </button>
               <button
                 onClick={handleNextClick}
-                 className="w-full flex items-center justify-center bg-primary cursor-pointer py-2 px-2 sm:px-3 transition-all hover:bg-black hover:text-white duration-300 border-2 border-black group"
+                className="w-full flex items-center justify-center bg-primary cursor-pointer py-2 px-2 sm:px-3 transition-all hover:bg-black hover:text-white duration-300 border-2 border-black group"
               >
                 <span className="text-[10px] sm:text-sm pl-2 pr-1 sm:pr-3">Next</span>
-                <Icon 
+                <Icon
                   icon="mdi:arrow-left"
                   className="rotate-180 transition-colors duration-300 text-black group-hover:text-white text-lg"
                   width="14"
--- app/register/details.jsx
+++ app/register/details.jsx
@@ -1,10 +1,17 @@
 'use client';
+
 import React, { useState, useEffect } from 'react';
 import { Icon } from '@iconify/react';
 import { motion } from 'framer-motion';
 
 const Details = ({ onNext, onPrevious, data, isFirstStep }) => {
-  const [formData, setFormData] = useState(data || { username: '', email: '' });
+  const [formData, setFormData] = useState(data || {
+    username: '',
+    email: '',
+    phone: '',
+    additionalInfo: '',
+    reasonToJoin: ''
+  });
 
   useEffect(() => {
     if (data) {
@@ -24,12 +31,11 @@ const Details = ({ onNext, onPrevious, data, isFirstStep }) => {
 
   return (
     <>
-    
       <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            className='min-h-[90vh] bg-emerald-50 p-4 md:p-8'>
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        className='min-h-[90vh] bg-emerald-50 p-4 md:p-8'>
         <h2 className="text-center my-[4.89rem] md:mb-14 sm:max-mb-11">Join with us!</h2>
         <form onSubmit={handleSubmit} className="max-w-lg mx-auto border-4 border-black bg-orange-50 p-4 md:p-6">
           <div className="w-full mb-10 mt-[3rem]">
@@ -46,8 +52,8 @@ const Details = ({ onNext, onPrevious, data, isFirstStep }) => {
               <label htmlFor="name" className="floating-placeholder">
                 Username
               </label>
-              <Icon 
-                icon="material-symbols:person" 
+              <Icon
+                icon="material-symbols:person"
                 className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500"
               />
             </div>
@@ -70,12 +76,64 @@ const Details = ({ onNext, onPrevious, data, isFirstStep }) => {
               >
                 Email
               </label>
-              <Icon 
+              <Icon
                 icon="material-symbols:mail-outline"
                 className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500"
               />
             </div>
           </div>
+          <div className="w-full mb-10">
+            <div className="relative">
+              <input
+                type="tel"
+                name="phone"
+                id="phone"
+                value={formData.phone}
+                onChange={handleChange}
+                className="w-full px-3 py-2 h-12 rounded-sm placeholder-transparent text-secondary-100 bg-emerald-50 text-sm border border-black focus:outline-none focus:border-2 peer transition-border"
+                placeholder=" "
+              />
+              <label htmlFor="phone" className="floating-placeholder">
+                Phone Number
+              </label>
+              <Icon
+                icon="mdi:phone"
+                className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500"
+              />
+            </div>
+          </div>
+
+          <div className="w-full mb-10">
+            <div className="relative">
+              <textarea
+                name="additionalInfo"
+                id="additionalInfo"
+                value={formData.additionalInfo}
+                onChange={handleChange}
+                className="w-full px-3 py-2 h-24 rounded-sm placeholder-transparent text-secondary-100 bg-emerald-50 text-sm border border-black focus:outline-none focus:border-2 peer transition-border"
+                placeholder=" "
+              />
+              <label htmlFor="additionalInfo" className="floating-placeholder">
+              Any other skills or contributions you would like to bring to the club?
+              </label>
+            </div>
+          </div>
+          <div className="w-full mb-10">
+            <div className="relative">
+              <textarea
+                name="reasonToJoin"
+                id="reasonToJoin"
+                value={formData.reasonToJoin}
+                onChange={handleChange}
+                className="w-full px-3 py-2 h-24 rounded-sm placeholder-transparent text-secondary-100 bg-emerald-50 text-sm border border-black focus:outline-none focus:border-2 peer transition-border"
+                placeholder=" "
+              />
+              <label htmlFor="reasonToJoin" className="floating-placeholder">
+                Why do you want to join CODESQUAD?
+              </label>
+            </div>
+          </div>
+
           <div className="flex justify-end">
             {!isFirstStep && (
               <button
@@ -83,7 +141,7 @@ const Details = ({ onNext, onPrevious, data, isFirstStep }) => {
                 onClick={onPrevious}
                 className="flex items-center justify-center bg-primary mb-10 cursor-pointer py-3 px-2 sm:px-3 transition-all hover:bg-black hover:text-white duration-300 border-2 border-black group"
               >
-                <Icon 
+                <Icon
                   icon="mdi:arrow-left"
                   className="transition-colors duration-300 text-black group-hover:text-white text-lg"
                   width="14"
@@ -97,10 +155,9 @@ const Details = ({ onNext, onPrevious, data, isFirstStep }) => {
               className="flex items-center justify-center w-full bg-primary mb-3 cursor-pointer py-3 px-3 sm:px-3 transition-all hover:bg-black hover:text-white duration-300 border-2 border-black group ml-auto"
             >
               <span className="text-[10px] sm:text-sm pr-1 sm:pr-3">Next</span>
-              <Icon 
+              <Icon
                 icon="mdi:arrow-right"
                 className="transition-colors duration-300 text-black group-hover:text-white text-lg"
-
               />
             </button>
           </div>
